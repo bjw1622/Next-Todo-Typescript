c1c7806264c580d8f543d43e685e657572125a4f
JaeWon Baek <bjw1622@gmail.com>
2022-12-13 16:50:46 +0900

    express server 추가🎉

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+const { default: Axios } = require("axios");
+const router = express.Router();
+
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(cors());
+
+let todos = [];
+
+// 목록 가져오기
+app.get("/todo", function (req, res) {
+  res.json(todos);
+});
+
+app.post("/todo", function (req, res) {
+  todos.push(req.body);
+  res.send(todos);
+});
+
+app.delete("/todoEntry", function (req, res) {
+  todos = [];
+});
+
+// app.delete("/todo", function (req, res) {
+//   const findInex = todos.find((todo) => todo.id === req.body.Id);
+//   const index = todos.indexOf(findInex);
+//   todos.splice(index, 1);
+//   res.json(todos);
+// });
+
+// app.put("/todoInputValue", function (req, res) {
+//   const findIndex = todos.find((todo) => todo.id === req.body.data.Id);
+//   findIndex.inputValue = req.body.data.InputValue;
+//   res.json(todos);
+// });
+
+// app.put("/todoCheckValue", function (req, res) {
+//   const findIndex = todos.find((todo) => todo.id === req.body.data.Id);
+//   findIndex.check = !findIndex.check;
+//   res.json(todos);
+// });
+
+app.listen(3001);
